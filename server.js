cda0e8a130a5830dfecfe27b214e7006540d0eca
sohom dey <sohomjuit@gmail.com>
2022-06-27 02:51:53 +0530

    almost done

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express=require('express');
 const app=express();
+const http=require('http').createServer(app);
 
-const PORT=process.env.PORT ||3000;
+const PORT=process.env.PORT ||5000;
 
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 })
-app.listen(PORT,()=>{
+http.listen(PORT,()=>{
     console.log(`server is running at port ${PORT}`);
 })
+
+//socket
+const io=require('socket.io')(http);
+io.on('connection',(socket)=>{
+    console.log('connected');
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg);
+    });
+});
